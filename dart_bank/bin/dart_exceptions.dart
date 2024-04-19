c545ef00b4e81ddfcdd2cd5d92b8a9c96bbb7ad1
Vilar95 <e.vilar95@gmail.com>
2024-04-19 17:05:12 -0300

    Try, catch and finally.

--- dart_bank/bin/dart_exceptions.dart
+++ dart_bank/bin/dart_exceptions.dart
@@ -1,25 +1,53 @@
+import 'dart:io';
+
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
 void main() {
-  // Criando o banco
-  BankController bankController = BankController();
 
-  // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+  // // Criando o banco
+  // BankController bankController = BankController();
+  // // Adicionando contas
+  // bankController.addAccount(
+  //     id: "Ricarth",
+  //     account:
+  //         Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+  // bankController.addAccount(
+  //     id: "Kako",
+  //     account:
+  //         Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+  // // Fazendo transferência
+  // bool result = bankController.makeTransfer(
+  //     idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+  // // Observando resultado
+  // print(result);
+  
+  print("Started main");
+  functionOne();
+  print("Finished Main");
+}
 
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+void functionOne(){
+  print("Started F01");
+  
+  try{
+    functionTwo();
+  }catch (exception, stackTrace){
+    print(exception);
+    print(stackTrace);
+    rethrow;
+  } finally{
+    print("Chegou no Finally");
+  }
+  print("Finished F01");
+}
 
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-  // Observando resultado
-  print(result);
+void functionTwo() {
+  print("Started F02");
+  for (var i = 1; i <= 5; i++) {
+    print(i);
+    double amount = double.parse("Not a number!");
+    
+  }
+  print("Finished F02");
 }
